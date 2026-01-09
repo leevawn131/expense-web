bd75e5e098db7adbee12259371675294daeb293a
leevawn131 <ledinhvan205@gmail.com>
2026-01-09 11:52:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,11 @@
-const form = document.getElementById("expense-form");
-const list = document.getElementById("list");
-const totalText = document.getElementById("total");
+function getExpenses() {
+  return JSON.parse(localStorage.getItem("expenses")) || [];
+}
 
-let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
-
-function render() {
-  list.innerHTML = "";
-  let total = 0;
-
-  expenses.forEach((e, i) => {
-    total += e.amount;
-    const li = document.createElement("li");
-    li.innerHTML = `
-      ${e.title} - ${e.amount} đ
-      <button onclick="remove(${i})">X</button>
-    `;
-    list.appendChild(li);
-  });
-
-  totalText.innerText = `Tổng: ${total} đ`;
+function saveExpenses(expenses) {
   localStorage.setItem("expenses", JSON.stringify(expenses));
 }
 
-function remove(index) {
-  expenses.splice(index, 1);
-  render();
+function getParam(name) {
+  return new URLSearchParams(window.location.search).get(name);
 }
-
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  const title = document.getElementById("title").value;
-  const amount = Number(document.getElementById("amount").value);
-
-  expenses.push({ title, amount });
-  form.reset();
-  render();
-});
-
-render();
